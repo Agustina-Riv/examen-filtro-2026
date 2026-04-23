f829636cc835681bac7fab8a2c9bdcf199241a9d
Agustina Rivoira <agustinarivoira7@gmail.com>
2026-04-23 20:09:22 -0300

    feat: add 2 function more canCastSpell and getPresentation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function1:
 const name = "Zara Darkbane";
 const characterClass = "Hechicera";
 let level = 7;
@@ -21,6 +22,7 @@ console.log(`Nombre: ${name}, Clase: ${characterClass}`);
 console.log(`Daño calculado contra un enemigo con 20 de defensa: ${calculateDamage(attack, 20)}`);
 console.log(`Daño calculado contra un enemigo con 60 de defensa: ${calculateDamage(attack, 60)}`);
 
+// Function2:
 
 const isAlive = (health) => health > 0;
 
@@ -28,3 +30,25 @@ console.log("--- Estado de Vida ---");
 console.log(`¿Está viva con 80 HP? ${isAlive(health)}`); 
 console.log(`¿Está viva con 0 HP? ${isAlive(0)}`);   
 console.log(`¿Está viva con -10 HP? ${isAlive(-10)}`); 
+
+// Function3:
+ 
+const canCastSpell = (currentMana, spellCost, isStunned) => {
+    if (currentMana >= spellCost && isStunned === false) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+console.log("¿Puede lanzar hechizo? (Caso exitoso):", canCastSpell(120, 50, false));
+console.log("¿Puede lanzar hechizo? (Aturdida):", canCastSpell(120, 50, true));
+
+// function4:
+
+function getPresentation(name, characterClass, level) {
+    return name + " — " + characterClass + " (Nivel " + level + ")";
+}
+
+console.log("Presentación del personaje:");
+console.log(getPresentation(name, characterClass, level));
