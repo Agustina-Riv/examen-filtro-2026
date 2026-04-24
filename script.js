e66f6c7cbdedf98ec89e4e71304b691f24b055c5
Agustina Rivoira <agustinarivoira7@gmail.com>
2026-04-23 21:17:12 -0300

    feat: changes on script.js to test the code

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Function1:
 const name = "Zara Darkbane";
 const characterClass = "Hechicera";
-let level = 7;
-let health = 80;
-let mana = 120;
-const attack = 45;
-const defense = 30;
+let level = 11;
+let health = 45;
+let mana = 30;
+const attack = 85;
+const defense = 50;
 
 function calculateDamage(attack, defense) {
     const damage = attack - defense;
